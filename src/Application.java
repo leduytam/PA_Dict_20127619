8bbe60dcc53c13421b3c207685650c2ee6715316
Lê Duy Tâm <leduytam600@gmail.com>
2022-11-20 15:07:40 +0700

    [refactor] set up project folder structure

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+public class Application implements Runnable {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Application());
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
